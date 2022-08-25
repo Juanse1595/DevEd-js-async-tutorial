ab34ac542860e25242b40ccf7b5ea7a6e4e004a8
Juanse1595 <jspbescos15@gmail.com>
2022-08-24 19:34:03 -0500

    ADD app.js contains example code of the explanation

---
+++ app.js
@@ -0,0 +1,12 @@
+// Sync code example
+
+function otherFunct() {
+  console.log("we are in otherFunc");
+  console.log("do some stuff");
+}
+
+console.log("Start");
+
+otherFunct();
+
+console.log("End");

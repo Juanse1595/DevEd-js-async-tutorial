56339c971a6ce130568fe2c8040870d5963896fa
Juanse1595 <3309@holberstonschool.com>
2022-08-25 21:23:12 -0500

    UPD app.js added videoDetails function to represent a nested function inside getUserVideos

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ console.log("End Async part");
 /* Example 2. in this case, when you run the code, user will be undefined until
 the end of the execution, the timeout will be executed last */
 
-console.log("Start");
+console.log('Start');
 
 function loginUser(email, password, callback) {
   setTimeout(() => {
-    console.log("now we have the data");
+    console.log('now we have the data');
     callback({ userEmail: email });
   }, 1500);
 }
@@ -46,28 +46,27 @@ function getUserVideos(email, callback){
 // version 2 getUserVideos
 function getUserVideos(email, callback) {
   setTimeout(() => {
-    callback(["video1", "video2", "video3"]);
-  }, 1000)
+    callback(['video1', 'video2', 'video3']);
+  }, 1000);
 }
 
 function videoDetails(videos, callback) {
   setTimeout(() => {
-    callback("Title of the video");
-  }, 1000)
+    callback('Title of the video');
+  }, 1000);
 }
 
-
-const user = loginUser("example@goomail.com", 123, (user) => {
+const user = loginUser('example@goomail.com', 123, (user) => {
   console.log(user);
   getUserVideos(user.email, (videos) => {
     console.log(videos);
     videoDetails(videos[0], (title) => {
       console.log(title);
-    })
-  })
+    });
+  });
 });
 
 // console.log(user); this will print undefined
 // console.log(getUserVideos()); this will also print undefined with the version 1
 
-console.log("End");
+console.log('End');

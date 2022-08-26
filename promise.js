824335f0a01434347a3d26ddd6ce3af19135e648
Juanse1595 <3309@holberstonschool.com>
2022-08-25 21:41:03 -0500

    UPD promise.js copied and paste example 2 from app.js the idea is to rewrite it with promises

--- promise.js
+++ promise.js
@@ -1,6 +1,6 @@
 /* Simple Promise example
 A Promise is an object that return a result on succes or on failure on an
-asynchronous operatoin*/
+asynchronous operatoin
 
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
@@ -16,3 +16,52 @@ promise
   .catch((err) => {
     console.log(err.message);
   });
+
+*/
+
+// Same example from app.js but with promises
+
+console.log('Start');
+
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log('now we have the data');
+    callback({ userEmail: email });
+  }, 1500);
+}
+
+/* version 1 getUserVideos
+function getUserVideos(email, callback){
+  setTimeout(() => {
+    return ["video1", "video2", "video3"];
+  }, 1000);
+}
+*/
+
+// version 2 getUserVideos
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(['video1', 'video2', 'video3']);
+  }, 1000);
+}
+
+function videoDetails(videos, callback) {
+  setTimeout(() => {
+    callback('Title of the video');
+  }, 1000);
+}
+
+const user = loginUser('example@goomail.com', 123, (user) => {
+  console.log(user);
+  getUserVideos(user.email, (videos) => {
+    console.log(videos);
+    videoDetails(videos[0], (title) => {
+      console.log(title);
+    });
+  });
+});
+
+// console.log(user); this will print undefined
+// console.log(getUserVideos()); this will also print undefined with the version 1
+
+console.log('End');
